22e57f0252c4a11ff2ef3c4449bde060940c52d5
MinhCuong99er <37433279+MinhCuong99er@users.noreply.github.com>
2018-05-11 13:39:51 +0700

    Update console2048.cpp

--- Exercise game 2048/console2048.cpp
+++ Exercise game 2048/console2048.cpp
@@ -38,3 +38,10 @@ void hideCursor()
     SetConsoleCursorInfo(hOut, &ConCurInf);
 }
 
+//void resizeConsole(int width, int height)
+//{
+//	HWND console = GetConsoleWindow();
+//	RECT r;
+//	GetWindowRect(console, &r);
+//	MoveWindow(console, r.left, r.top, width, height, TRUE);
+//}
