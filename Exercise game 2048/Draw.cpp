6dd83d97b80e503926c3c155dd548eb4e346b57f
MinhCuong99er <37433279+MinhCuong99er@users.noreply.github.com>
2018-05-11 13:35:12 +0700

    Update Draw.cpp

--- Exercise game 2048/Draw.cpp
+++ Exercise game 2048/Draw.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 const string Logo =
-    "\n\n\n\n\n                                                         \n"
+    "\n\n\n\n\n\n\n\n\n\n\n\n                                           \n"
     "\t\t  2 2 2           0 0 0                 4 4         8 8 8 8 8  \n"
     "\t\t2       2       0       0             4   4       8           8\n"
     "\t\t        2     0           0         4     4       8           8\n"
@@ -15,35 +15,36 @@ const string Logo =
     "\t\t  2             0       0                 4       8           8\n"
     "\t\t2 2 2 2 2         0 0 0                   4         8 8 8 8 8  \n";
 const string Guide =
-    "\t\t\tGame instructions:\n\n"
-    "\t<!> Use the navigation buttons to move:\n\n"
-    "\t\t+ Up:    W \n\n"
-    "\t\t+ Down:  S \n\n"
-    "\t\t+ Right: D \n\n"
-    "\t\t+ Left:  A \n\n"
-    "\t<!> Press ESC to exit the game.\n\n"
-    "\t\t\tGood luck !!! (^_^)\n\n\n"
-    "\t\tREADY!!! GO  --->>> ";
+    "\n\n\n\n\n\n\n\n\n                       \n\n"
+    "\t\t\t\tGame instructions:                 \n\n"
+    "\t\t<!> Use the navigation buttons to move:\n\n"
+    "\t\t\t+ Up:    W                           \n\n"
+    "\t\t\t+ Down:  S                           \n\n"
+    "\t\t\t+ Right: D                           \n\n"
+    "\t\t\t+ Left:  A                           \n\n"
+    "\t\t<!> Press ESC to exit the game.        \n\n"
+    "\t\t<!> Press R to replay the game.        \n\n"
+    "\t\t\t\tGood luck !!! (^_^)\n              \n\n"
+    "\t\t\tREADY!!! GO  --->>> ";
 const string BoardHand=
-    "  +----------------------------------+   \n"
-    "\t\t\t\t      |                                  |      \n"
-    "\t\t\t\t      |        Best Score:               |      \n"
-    "\t\t\t\t      |                                  |      \n"
-    "\t\t\t\t    _ |        Your Score:               | _    \n"
-    "\t\t\t\t   (o)|                                  |(o)   \n"
-    "\t\t\t\t  ( { |                                  | } )  \n"
-    "\t\t\t\t ( (_ |                                  | _) ) \n"
-    "\t\t\t\t((# {)+----------------------------------+(} #))\n"
-    "\t\t\t\t(### {_} }                            { {_} ###)\n"
-    "\t\t\t\t {      }                              {      } \n"
-    "\t\t\t\t  {____}                                {____}  \n"
-    "\t\t\t\t  |    |                                |    |  \n"
-    "\t\t\t\t  |    |                                |    |  \n"
-    "\t\t\t\t  |    |                                |    |  \n"
-    "\t\t\t\t  |    |                                |    |  \n"
-    "\t\t\t\t  |    |                                |    |  \n"
-    "\t\t\t\t  (____)                                (____)  \n";
-
+    "\t\t      +-------------------------------------+   \n"
+    "\t\t      |                                     |      \n"
+    "\t\t      |        Best Score:                  |      \n"
+    "\t\t      |                                     |      \n"
+    "\t\t    _ |        Your Score:                  | _    \n"
+    "\t\t   (o)|                                     |(o)   \n"
+    "\t\t  ( { |                                     | } )  \n"
+    "\t\t ( (_ |                                     | _) ) \n"
+    "\t\t((# {)+-------------------------------------+(} #))\n"
+    "\t\t(### {_} }                               { {_} ###)\n"
+    "\t\t {      }                                 {      } \n"
+    "\t\t  {____}                                   {____}  \n"
+    "\t\t  |    |                                   |    |  \n"
+    "\t\t  |    |                                   |    |  \n"
+    "\t\t  |    |                                   |    |  \n"
+    "\t\t  |    |                                   |    |  \n"
+    "\t\t  |    |                                   |    |  \n"
+    "\t\t  (____)                                   (____)  \n";
 void setText(string text)
 {
     while(1)
@@ -86,44 +87,32 @@ void setColour(int &colour)
         default:    textColor(4);
     }
 }
-void drawTheFrame()
+void draw()
 {
-    gotoxy(36,0);
+    gotoxy(0,0);
+	for(int i=0;i<=BoardWidth;i++)
+	{
+		for(int j=0;j<=BoardLength;j++)
+		{
+		    gotoxy(j+29,i+2);
+			if (i%4==0 || j%6==0)
+            {
+                textColor(2);
+                cout << char (219);
+            }
+			else
+            {
+                textColor(0);
+                cout << char (219);
+            }
+		}
+		cout<<endl;
+	}
+	gotoxy(0,20);
     textColor(3);
     cout<<BoardHand;
-    textColor(2);
-
-    for(int i=0;i<=BoardWidth;i++)
-    {
-        for(int j=0;j<=BoardLength;j++)
-        {
-        	gotoxy(j,i);
-            if(i==0 && j==0)
-                cout<<char(-55);
-            else if(i==16 && j==0)
-                cout<<char(-56);
-            else if(i==0 && j==24)
-                cout<<char(-69);
-            else if(i==16 && j==24)
-                cout<<char(-68);
-            else if((i==4 || i==8 || i==12) && j==0)
-                cout<<char(-52);
-            else if((i==4 || i==8 || i==12) && j==24)
-                cout<<char(-71);
-            else if(i==0 && (j==6 || j==12 || j==18))
-                cout<<char(-53);
-            else if(i==16 && (j==6 || j==12 || j==18))
-                cout<<char(-54);
-            else if((i==4 || i==8 || i==12) && (j==6 || j==12 || j==18))
-                cout<<char(-50);
-            else if(i==0 || i==4 || i==8 || i==12 || i==16)
-                cout<<char(-51);
-            else if(j==0 || j==6 || j==12 || j==18 || j==24)
-                cout<<char(-70);
-        }
-    }
 }
-void fillTheBoard(int matrix[4][4], int &score, int &bestScore)
+void display(int matrix[4][4], int &score, int &bestScore)
 {
     for(int i=0;i<4;i++)
     {
@@ -132,20 +121,20 @@ void fillTheBoard(int matrix[4][4], int &score, int &bestScore)
             setColour(matrix[i][j]);
             if (matrix[i][j]<100 && matrix[i][j]!=0)
             {
-                gotoxy(3+j*6,2+i*4);
+                gotoxy(32+j*6,4+i*4);
                 cout<<matrix[i][j];
             }
             else if ((matrix[i][j]>100 && matrix[i][j]!=0))
             {
-                gotoxy(2+j*6,2+i*4);
+                gotoxy(31+j*6,4+i*4);
                 cout<<matrix[i][j];
             }
         }
     }
+    gotoxy(43,24);
     textColor(1);
-    gotoxy(59,4);
     cout<<score;
     textColor(4);
-    gotoxy(59,2);
+    gotoxy(43,22);
     cout<<bestScore;
 }
