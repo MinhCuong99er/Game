c3a7a0be7e576a25248f0398a21649d9ec2f9cd7
MinhCuong99er <37433279+MinhCuong99er@users.noreply.github.com>
2018-05-11 13:50:01 +0700

    Update Draw.h

--- Exercise game 2048/Draw.h
+++ Exercise game 2048/Draw.h
@@ -7,6 +7,22 @@ using namespace std;
 
 #define BoardLength 24
 #define BoardWidth 16
+#define ColorCode_Back			0
+#define ColorCode_DarkBlue		1
+#define ColorCode_DarkGreen		2
+#define ColorCode_DarkCyan		3
+#define ColorCode_DarkRed		4
+#define ColorCode_DarkPink		5
+#define ColorCode_DarkYellow	6
+#define ColorCode_DarkWhite		7
+#define ColorCode_Grey			8
+#define ColorCode_Blue			9
+#define ColorCode_Green			10
+#define ColorCode_Cyan			11
+#define ColorCode_Red			12
+#define ColorCode_Pink			13
+#define ColorCode_Yellow		14
+#define ColorCode_White			15
 
 const string Face =
     "\n\n\n\n\n\t\t                \n"
